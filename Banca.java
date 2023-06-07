bca4563f60a045dae19c18c81c904559e9bd78c3
PiriNTT <Pietro.Giardina@nttdata.com>
2023-06-07 15:29:23 +0200

    Si vado subito

--- Banca.java
+++ Banca.java
@@ -1,3 +1,45 @@
 public class Banca {
-    private String
+    private String nome;
+    private double saldo;
+    public Banca(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+        this.saldo = 0;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    public void aggiungiSaldo(double i) {
+        saldo += i;
+    }
+    public void prelevaSaldo(double i) {
+        if(saldo > i) {
+            saldo -= i;
+        } else {
+            System.out.println("Il saldo diventerebbe negativo. Non prelevo niente");
+        }
+    }
+    public void effettuaTransazione(double importo, boolean prelievo) {
+        if(prelievo) {
+            prelevaSaldo(importo);
+        } else {
+            aggiungiSaldo(importo);
+        }
+    }
+    public void mostraSaldo() {
+        System.out.println(this.saldo);
+    }
+
 }
