fed5334593e51b29b259ad182c373c521fcaac21
PiriNTT <Pietro.Giardina@nttdata.com>
2023-06-07 15:33:07 +0200

    Si vado subito

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Banca banca1 = new Banca("Banca_XYZ" , 100000);
+        Banca banca1 = new Banca("Banca_XYZ");
         banca1.mostraSaldo();
         banca1.effettuaTransazione(1000,false);
         banca1.effettuaTransazione(500,true);
