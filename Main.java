384e1fef746303b6b40347ee84e4c4a96af5c614
Giuseppe <giuseppeprudente20@gmail.com>
2023-06-07 15:27:36 +0200

    vai a kagglare Pietro!

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        Banca banca1 = new Banca("Banca_XYZ" , 100000);
+        banca1.mostraSaldo();
+        banca1.effettuaTransazione(1000,false);
+        banca1.effettuaTransazione(500,true);
+        banca1.mostraSaldo();
 
     }
 }
